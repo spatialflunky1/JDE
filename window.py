867fabb9c7546116e9dd9ef6627cd4f56880b6b0
Spatialflunky1 <53266404+spatialflunky1@users.noreply.github.com>
2020-02-13 17:40:25 -0500

    The IDE itself
    Version A0.2

---
+++ window.py
@@ -0,0 +1,32 @@
+from tkinter import *
+import time
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.default_speed = 10
+        self.draw()
+    def close_window(self):
+        self.master.destroy()
+    def draw(self):
+        self.master.title("IDE Withought a Name")
+        self.pack(fill=BOTH, expand=1)
+        quitButton = Button(self, text="Exit", command = self.close_window)
+        quitButton.place(x=0, y=0)
+
+root = Tk()
+scrollbar = Scrollbar(root)
+side_scrollbar = Scrollbar(root, orient="horizontal")
+textbox = Text(root, height=73, width=70)
+scrollbar.pack(side=RIGHT, fill=Y)
+side_scrollbar.pack(side=BOTTOM, fill=X)
+textbox.pack(side=BOTTOM, fill=BOTH, expand=1)
+scrollbar.config(command=textbox.yview)
+side_scrollbar.config(command=textbox.xview)
+textbox.config(yscrollcommand=scrollbar.set)
+textbox.config(xscrollcommand=side_scrollbar.set)
+textbox.insert(END, "Click here to type\n")
+root.geometry("1920x1080")
+app = Window(root)
+app.draw()
+root.mainloop()
