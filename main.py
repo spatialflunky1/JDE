9826f8d8ffc36c6231478ba11ddb230dd3b60198
Jake <mjandrus06@gmail.com>
2020-05-15 23:58:25 -0400

    added function to copy, cut, paste, and select all

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import tkinter.filedialog
 import time
 import window
 import uimanager
+def popup(event):
+    rightclickmenu.tk_popup(event.x_root, event.y_root)
 def retrieve():
     input = textbox.get("1.0",END)
     return input
@@ -37,8 +39,19 @@ def new():
     global app
     app.settitle("NewFile")
     textbox.delete(1.0, END)
-def popup(event):
-    rightclickmenu.tk_popup(event.x_root, event.y_root)
+def copy(event=None):
+    root.clipboard_clear()
+    text = root.selection_get()
+    root.clipboard_append(text)
+def paste(event=None):
+    text = root.selection_get(selection='CLIPBOARD')
+    textbox.insert('insert', text)
+    textbox.delete("sel.first", "sel.last")
+def cut(event=None):
+    copy()
+    textbox.delete("sel.first", "sel.last")
+def select_all(event=None):
+    textbox.tag_add('sel', '1.0', 'end')
 ui_manager = uimanager.UIManager(frame = [800,600])
 # print(ui_manager.is_fullscreen)
 filepath = ""
@@ -52,16 +65,20 @@ filemenu.add_command(label="Save As...", command=lambda : saveas())
 filemenu.add_command(label="Exit", command=root.quit)
 menubar.add_cascade(label="File", menu=filemenu)
 editmenu = Menu(menubar, tearoff=0)
-editmenu.add_command(label="Copy")
-editmenu.add_command(label="Paste")
-editmenu.add_command(label="Undo")
-editmenu.add_command(label="Redo")
-editmenu.add_command(label="Find")
-editmenu.add_command(label="Find & Replace")
+editmenu.add_command(label="Copy                     Ctrl + C", command =lambda : copy())
+editmenu.add_command(label="Cut                        Ctrl + Z", command =lambda :cut())
+editmenu.add_command(label="Paste                     Ctrl + V", command =lambda : paste())
+editmenu.add_command(label="Select All               Ctrl + A", command =lambda :select_all())
+editmenu.add_command(label="Undo                     Ctrl + Z")
+editmenu.add_command(label="Redo                      Ctrl + Y")
+editmenu.add_command(label="Find                       Ctrl + F")
+editmenu.add_command(label="Find & Replace    Ctrl + Shift + F")
 menubar.add_cascade(label="Edit", menu=editmenu)
 rightclickmenu = Menu(menubar, tearoff=0)
-rightclickmenu.add_command(label="Copy")
-rightclickmenu.add_command(label="Paste")
+rightclickmenu.add_command(label="Copy", command =lambda : copy())
+rightclickmenu.add_command(label="Cut", command =lambda : cut())
+rightclickmenu.add_command(label="Paste", command =lambda: paste())
+rightclickmenu.add_command(label="Select All", command =lambda :select_all())
 rightclickmenu.add_command(label="Undo")
 rightclickmenu.add_command(label="Redo")
 root.bind("<Button-3>", popup)
